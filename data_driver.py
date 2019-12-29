79179c1e64c34f308e0c361138a76d76e485c1e4
Hoss <hoseinhonarvar@gmail.com>
2019-12-29 17:04:07 -0500

    first commit

--- data_driver.py
+++ data_driver.py
@@ -1,11 +1,10 @@
-#from preprocessing import preprocessing_driver #import *
-from preprocessing.preprocessing_driver import Feature_Selection
+from utils.data_preprocessing.data_preparation import data_preparation as d_p
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Get the list of companies from ./data/dow30.csv
-stocks = data_creater.companies()
+stocks = d_p.companies()
 print(stocks)
 
 # Get the symbol of companies from /data/dow30.csv
@@ -16,7 +15,7 @@ start_date, end_date = '20161101', '20181031'
 
 # Download quotes from yahoo and save to ./data/{company symbol}/quotes.csv
 for ticker in symbols:
-    download = data_creater.Downloader(ticker, start_date, end_date)
+    download = d_p.Downloader(ticker, start_date, end_date)
     download.save()
     
 
@@ -28,14 +27,14 @@ for ticker in symbols:
     # Check if file exist first
     if True: #os.path.isfile(file_path.format(ticker)):
         
-        F_S = Feature_Selection(ticker, pd.read_csv(file_path.format(ticker)))
+        F_S = d_p.Feature_Selection(ticker, pd.read_csv(file_path.format(ticker)))
         F_S.calculate_features()
         F_S.normalize_data()
         F_S.save_stock_data() # save to ./data/{company symbol}/quote_processed.csv
         F_S.save_normalized_data() # save to ./data/{company symbol}/normalized.csv
         
 
-vols = [data_creater.Volatility(ticker).annual for ticker in symbols]
+vols = [d_p.Volatility(ticker).annual for ticker in symbols]
 dataset = pd.DataFrame({'Symbol':symbols,'Volatility':vols})
 
 # Volatility distribution
--- inference_driver.py
+++ inference_driver.py
@@ -1,15 +1,16 @@
-from inference.model import Predictor
-from preprocessing.preprocessing_driver import *
+from core.model_inference.model_inference import Inference
 
-tickers = ['GOOG']
+tickers = ['MMM']
 start_date = '20161101'
 end_date = '20181031'
 #download quotes from yahoo and save to directory
+
 for ticker in tickers:
-    Predictor.download_prep(ticker,start_date,end_date)
+    print(ticker)
+    Inference.download_prep(ticker,start_date,end_date)
 
 stock_1 = tickers[0]
 print("***  Closing price prediction for {} *** ".format(stock_1))
-pred = Predictor(stock_1)
-pred.select_model(verbose=1)
+pred = Inference(stock_1)
+pred.select_model(verbose=1, tickers=tickers)
 pred.graph()
